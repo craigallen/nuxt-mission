cdd781348e85d3e1bb0ad475223627d7e6a697c0
Craig Allen <craig@openwavesdesign.com>
2021-04-28 15:01:10 -0400

    added GitHub link

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,11 @@
     <div>
       <Logo />
       <h1 class="title">About</h1>
+      <p>
+        <a href="https://github.com/craigallen/nuxt-mission">
+          View this project on GitHub
+        </a>
+      </p>
     </div>
   </div>
 </template>
@@ -10,36 +15,3 @@
 <script>
 export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
